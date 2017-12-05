e20fefadfe0071b19aecd5dd11a7b675969582c6
pieterlouw <pieterlouw@gmail.com>
2017-12-05 13:38:52 +0200

    Added interceptors

--- client/main.go
+++ client/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"crypto/tls"
+	"encoding/base64"
 	"flag"
 	"fmt"
 	"log"
@@ -14,6 +15,8 @@ import (
 )
 
 var target = flag.String("l", ":7100", "Specify the port that the server is listening on")
+var username = flag.String("u", "scaramoucheX2", "Specify the username to auth the call")
+var password = flag.String("p", "Can-you-do-the-fandango?", "Specify the password to auth the call")
 
 type byVersion []*pb.ReleaseInfo
 
@@ -30,7 +33,15 @@ func main() {
 		InsecureSkipVerify: true, // using self signed certificate for demo, for more secure connections see https://bbengfort.github.io/programmer/2017/03/03/secure-grpc.html
 	})
 
-	conn, err := grpc.Dial(*target, grpc.WithTransportCredentials(creds))
+	grpcAuth := &basicAuthCreds{
+		username: *username,
+		password: *password,
+	}
+
+	conn, err := grpc.Dial(*target,
+		grpc.WithTransportCredentials(creds),
+		grpc.WithPerRPCCredentials(grpcAuth),
+	)
 	if err != nil {
 		log.Fatalf("grpc.Dial err: %v", err)
 	}
@@ -55,4 +66,29 @@ func main() {
 	for _, ri := range releases {
 		fmt.Printf("%s\t%s\t%s\n", ri.GetVersion(), ri.GetReleaseDate(), ri.GetReleaseNotesURL())
 	}
+}
+
+// basicAuthCreds is an implementation of credentials.PerRPCCredentials
+// that transforms the username and password into a base64 encoded value similar
+// to HTTP Basic xxx
+type basicAuthCreds struct {
+	username, password string
+}
+
+// GetRequestMetadata sets the value for "authorization" key
+func (b *basicAuthCreds) GetRequestMetadata(context.Context, ...string) (map[string]string, error) {
+	return map[string]string{
+		"authorization": "Basic " + basicAuth(b.username, b.password),
+	}, nil
+}
+
+// RequireTransportSecurity should be true as even though the credentials are base64, we want to have it encrypted over the wire.
+func (b *basicAuthCreds) RequireTransportSecurity() bool {
+	return true
+}
+
+//helper function
+func basicAuth(username, password string) string {
+	auth := username + ":" + password
+	return base64.StdEncoding.EncodeToString([]byte(auth))
 }
